129e7ad59d4c73dbb96f2837cfdaa6a674db005b
Adam Lancaster <lancaster28@hotmail.com>
2016-11-30 00:43:52 +0000

    cont

--- spec/available_moves_spec.rb
+++ spec/available_moves_spec.rb
@@ -2,14 +2,15 @@ require 'spec_helper'
 require 'available_moves.rb'
 
 RSpec.describe AvailableMoves do
-  describe "#second_players_first_moves" do
-    FIRST_MOVES = ["0,0","0,1", "0,2","1,0", "1,1", "1,2" ,"2,0" ,"2,1", "2,2"]
+  describe "#available_positions" do
+    FIRST_MOVES = [*0..8]
 
     it 'returns all possible move combinations for any given starting position' do
       starting_position = FIRST_MOVES.sample
-      expect(subject.second_players_first_moves(starting_position)).to eq FIRST_MOVES - [starting_position]
+      expect(subject.available_positions(starting_position)).to eq FIRST_MOVES - [starting_position]
     end
   end
+
   describe "#state_after_each_first_move" do
     xit 'returns us all the possible states after one move each, given a first move' do
       expect(subject.state_after_each_first_move(FIRST_MOVES.sample))
