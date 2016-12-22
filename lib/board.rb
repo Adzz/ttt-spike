293038b40168927b37a8820c20a2ad78cc2974d1
Adam Lancaster <lancaster28@hotmail.com>
2016-12-22 18:51:58 +0000

    Close to having a board drawn and centered

--- lib/board.rb
+++ lib/board.rb
@@ -1,24 +1,26 @@
 class Board
-  STARTING_POSITIONS = [*0..8]
-  WINNING_LINES = [
-    [0,1,2],
-    [3,4,5],
-    [6,7,8],
-    [0,3,6],
-    [2,4,6],
-    [0,4,8],
-    [1,4,7],
-    [2,5,8]
-  ]
-
-  def available_positions(taken_positions)
-    STARTING_POSITIONS - [taken_positions]
+  def initialize
+    @board_lines = [
+      "   |    |   ",
+      "___|____|___",
+      "   |    |   ",
+      "___|____|___",
+      "   |    |   ",
+      "   |    |   "
+    ]
   end
 
-  def winning_board?(board, player)
-    WINNING_LINES.each do |line|
-      return true if board.values_at(*line) == [player, player, player]
-    end
-    false
+  def height
+    board_lines.count
   end
+
+  def width
+     board_lines.first.length
+  end
+
+  def board
+    board_lines.join("\n").to_s
+  end
+
+  attr_reader :board_lines
 end
--- views/game.rb
+++ views/game.rb
@@ -1,13 +1,18 @@
 require_relative 'window.rb'
+require_relative '../lib/board.rb'
 
 class Game < Window
   include Curses
 
   def screen
     window.refresh
+    noecho
     window.box("|", "-")
-    board(60,"|","~")
+    board = board(60,"|","~")
     window.refresh
+    Board.new.board_lines.each_with_index do |board_line, index|
+      position_and_type_from_center(board_line, -index)
+    end
     getstr
   end
 
@@ -18,3 +23,11 @@ class Game < Window
     board.box(vertical_border, horizontal_border)
   end
 end
+
+# to do
+#  draw the actual board
+#     6 subwindows? Positioned? then have the cursor move to each one?
+#     have the character be placed in the center of it
+# moving the cursor around.
+# have enter be the choose move
+# have the AI move show up somehow?!
