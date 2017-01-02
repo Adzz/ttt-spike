4b55be632d0fcc0b6d99a68a8c82ac1322ad3900
Adam Lancaster <lancaster28@hotmail.com>
2017-01-02 16:09:41 +0000

    stashing changes for now

--- lib/commands/client.rb
+++ lib/commands/client.rb
@@ -2,13 +2,19 @@ require_relative 'invoker.rb'
 require_relative '../../views/game/one_player_game.rb'
 
 class Client
-  def initialize
+  def initialize(view)
     @invoker = Invoker.new
-    @one_player_game = OnePlayerGame.new
+    @view = view
   end
 
-  def take_move
-    
+  def place_mark
+    command = getch
+    if return_key.include?(command)
+      place_mark unless one_player_game.cursor_within_board?
+      one_player_game.board.update_state(cursor_position_on_board, player)
+      one_player_game.render_board
+      one_player_game.computer_move
+    end
   end
 
   private
